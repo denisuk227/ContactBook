82423ae72e1f4ee7fb9679719e7668c704929f1e
denisuk227 <45741180+denisuk227@users.noreply.github.com>
2018-12-10 00:04:48 +0300

    Update
    ContactBook v1.0

--- Project.py
+++ Project.py
@@ -1,5 +1,6 @@
 #Название нашего чудо-софта
-print('Innovation contact searcher\nby DENISUK')
+print('Innovation ContactBook\nby DENISUK')
+print(' ')
 #Словарь, в котором содержутся наши контакты
 contacts = {
   'Чмо-Кирюша':'+79169326664',
@@ -12,7 +13,14 @@ contacts = {
 def search():
   contact_searcher_input = input('Введите имя контакта: ')
   result = 'Номер телефона контакта ' + str(contact_searcher_input) + ': ' + str(contacts[contact_searcher_input])
-  return result
+  print(' ')
+  print(result)
+  print(' ')
+  checker1 = int(input('Хочешь сделать ещё что-нибудь? Введи 1: '))
+  print(' ')
+  if checker1 == 1:
+    start_of_program()
+  checker1 = 0
   
 #Функция добавления контакта (Функция №2)
 def add_contact():
@@ -20,17 +28,61 @@ def add_contact():
   contact_phone = input('Введите телефон нового контакта: ')
   new_contact = {contact_name : contact_phone}
   contacts.update(new_contact)
+  print(' ')
   print('Контакт с именем ' + contact_name + ' успешно добавлен')
+  print(' ')
+  checker2 = int(input('Хочешь сделать ещё что-нибудь? Введи 1: '))
+  print(' ')
+  if checker2 == 1:
+    start_of_program()
+  checker2 = 0
+
+#Взламываем наса за 0.000001 секунду (Функция №3)
+def hack_the_NASA():
+  for counter in range(10):
+    print('NASA HAS BEEN HACKED!')
+  print(' ')
+  checker4 = int(input('Хочешь сделать ещё что-нибудь? Введи 1: '))
+  print(' ')
+  if chekcher4 == 1:
+    start_of_program()
+  checker4 = 0
+
+#Функция, показывающая имена всех контактов (Функция №4)
+def show_contacts():
+  print('Доступные контакты: ')
+  print(' ')
+  for i in contacts:
+    print(i)
+  print(' ')
+  checker3 = int(input('Хочешь сделать ещё что-нибудь? Введи 1: '))
+  print(' ')
+  if chekcher3 == 1:
+    start_of_program()
+  checker3 = 0
+
+
 
 #Начало программы для пользователя
-print('Что ты хочешь, чмошник?: ')
-print('1 - Найти контакт\n2- Добавить новый контактn\3 - Взломать НАСА\n4 - Вывести все контакты')
+def start_of_program():
+  print('Что ты хочешь, чмошник?: ')
+  print('1 - Найти контакт\n2 - Добавить новый контакт\n3 - Взломать НАСА\n4 - Вывести все контакты')
+  #Переменная, в которой мы считываем желание пользователя
+  wuw = int(input())
+  if wuw == 1:
+    search()
+  if wuw == 2:
+    add_contact()
+  if wuw == 3:
+    hack_the_NASA()
+  if wuw == 4:
+    show_contacts()
+  checker4 = int(input('Хочешь сделать ещё что-нибудь? Введи 1: '))
+  print(' ')
+  if checker4 == 1:
+    start_of_program()
+  checker4 = 0
 
-#WhatUserWant - переменная, в которой мы считываем желание пользователя
 
-wuw = int(input())
-
-if wuw == 1:
-  print(search())
-if wuw == 2:
-
+#Запускаем наше детище
+start_of_program()
